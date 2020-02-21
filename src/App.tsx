49efe43303104eb5b2358342d583478742707c27
fuyongbo <fuyongbo@everjiankang.com>
2020-02-21 11:42:19 +0800

    测试 格式

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component({})
 export default class App extends Vue {
   public render () {
-    console.log('test app')
+    console.log('test app');
     return (
       <router-view class={ 'app-' + this.$route.name }/>
     )
