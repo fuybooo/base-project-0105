65d05ba88166166cd8e2f538af64992911625dc7
fuyongbo <fuyongbo@everjiankang.com>
2020-02-21 11:50:27 +0800

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Component, Vue } from 'vue-property-decorator'
 export default class App extends Vue {
   public render () {
     console.log('test app');
+    console.log('test app');
     return (
       <router-view class={ 'app-' + this.$route.name }/>
     )
